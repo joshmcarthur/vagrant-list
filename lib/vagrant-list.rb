0c1f0718ab7905f8ad37569caa82a5008290f266
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-14 10:52:40 +1200

    Move core class functionality out of vagrant-list.rb and update for newer versions of Vagrant

--- lib/vagrant-list.rb
+++ lib/vagrant-list.rb
@@ -1,29 +1,6 @@
 require "vagrant-list/version"
-require "vagrant/ext/driver/driver_overrides"
-require "vagrant/vm_info"
+require "vagrant/list/vm_info"
 require "vagrant"
 require "pp"
 
-module Vagrant
-  module List
-    class All < ::Vagrant::Command::Base
-      def execute
-        all = Driver::VirtualBox.new(nil).read_vms(:vms)
-        @env.ui.info "ALL:"
-        all.each do |uuid|
-          @env.ui.info Vagrant::VMInfo.new(uuid).inspect
-        end
-
-        running = Driver::VirtualBox.new(nil).read_vms(:runningvms)
-        @env.ui.info "RUNNING:"
-        running.each do |uuid|
-          @env.ui.info Vagrant::VMInfo.new(uuid).inspect
-        end
-      end
-    end
-  end
-end
-
-Vagrant::Driver::VirtualBox_4_1.send(:include, Vagrant::Ext::Driver::DriverOverrides)
-Vagrant::Driver::VirtualBox_4_2.send(:include, Vagrant::Ext::Driver::DriverOverrides)
-Vagrant.commands.register(:list) { Vagrant::List::All }
+VagrantPlugins::ProviderVirtualBox::Driver::Base.send(:include, Vagrant::List::Decorators::DriverDecorator)
