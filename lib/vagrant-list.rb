52defe2637539324b584138fa0fdac654035a1a1
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-14 12:55:02 +1200

    Require correct files (including i18n), which VagrantError needs to build appropriate messages

--- lib/vagrant-list.rb
+++ lib/vagrant-list.rb
@@ -1,6 +1,6 @@
+require 'i18n'
 require "vagrant-list/version"
-require "vagrant/list/vm_info"
-require "vagrant"
-require "pp"
+require "vagrant-list/vm_info"
+require "vagrant-list/command_error"
+require "vagrant-list/virtual_box/list"
 
-VagrantPlugins::ProviderVirtualBox::Driver::Base.send(:include, Vagrant::List::Decorators::DriverDecorator)
