9af6d731a38d90a394c508c6a666f4976f9f4a23
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-14 13:00:49 +1200

    Require plugin file in main gem file

--- lib/vagrant-list.rb
+++ lib/vagrant-list.rb
@@ -1,5 +1,6 @@
 require 'i18n'
 require "vagrant-list/version"
+require "vagrant-list/plugin"
 require "vagrant-list/vm_info"
 require "vagrant-list/command_error"
 require "vagrant-list/virtual_box/list"
