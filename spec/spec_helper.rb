7ae0cb3db8ee0326236732b1de868198560e347d
Josh McArthur <joshua.mcarthur@gmail.com>
2013-04-14 12:55:46 +1200

    Add warning message about requiring having a test vm running

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,3 +3,12 @@ require 'bundler/setup'
 require 'vagrant-list'
 
 
+puts <<-WARNING
+WARNING: Because there is currently no support for mocking or stubbing
+out calls to VirtualBox, it is required that there be at least
+one running VM called 'Test' loaded into VirtualBox in order
+for these tests to pass.
+  WARNING
+
+
+
